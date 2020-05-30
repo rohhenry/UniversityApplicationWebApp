e960f3164c0fb01aa09ed127d68a87626148a964
ElisaHu <44733930+ElisaHu@users.noreply.github.com>
2020-05-30 13:56:10 -0700

    Create pdo_constructor.php

---
+++ src/pdo_constructor.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * Uncomment this line for local development.
+ * Comment out this line when uploading to the server.
+ */
+$pdo=new PDO('mysql:host=localhost;dbname=zyxwstorage','username','password');
+
+?>
