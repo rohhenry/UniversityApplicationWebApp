b344249ee94e3e63058b5c92100c27afcd15749b
g9i2b <henryroh2000@gmail.com>
2020-06-07 15:56:28 -0700

    recruiterlogin.php

--- src/StudentLogin.php
+++ src/StudentLogin.php
@@ -1,5 +1,8 @@
 <?php 
     require_once "pdo_constructor.php";
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 ?>
 
 <!DOCTYPE HTML>
@@ -9,5 +12,7 @@
     <input type="text" name="username">
     <p for="password">Password:</p>
     <input type="password" name="password">
+    <br>
+    <input type="submit" value="Sign In">
 </form>
 </html>
