9d012e974654ffdde00f293c484aad348860b193
g9i2b <henryroh2000@gmail.com>
2020-06-07 13:29:12 -0700

    changed StudentLogin

---
+++ src/StudentLogin.php
@@ -0,0 +1,13 @@
+<?php 
+    require_once "pdo_constructor.php";
+?>
+
+<!DOCTYPE HTML>
+<html>
+<form method="post">
+    <p>Username:</p>
+    <input type="text" name="username">
+    <p for="password">Password:</p>
+    <input type="password" name="password">
+</form>
+</html>
