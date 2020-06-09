1ca585c3a76b692ab2e0a14ed2c231f5d8aa81d0
jungwook Jang <jjjw0529@gmail.com>
2020-06-08 16:14:52 -0800

    login with slide show

--- src/StudentLogin.php
+++ src/StudentLogin.php
@@ -34,20 +34,27 @@
    
 ?>
 
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welocome To CPSC304</title>
+    <link rel="stylesheet" href="../css/wPage.css">
+</head>
 <body>
-    <div class="slide_bg">
-        <div id="login_form">
-            <form method="post">
-                <p>Username:</p>
-                <input type="text" name="username">
-                <p for="password">Password:</p>
-                <input type="password" name="password">
-                <br>
-                <input type="submit" value="Sign In">
-            </form>
-        </div>
+<div class = "slide_bg">
+    <div id="login_form">
+        <form>
+            <p>Username:</p>
+            <input type="text" name="username">
+            <p for="password">Password:</p>
+            <input type="password" name="password">
+            <br>
+            <input type="submit" value="Sign In">
+        </form>
     </div>
+</div>
 </body>
 </html>
