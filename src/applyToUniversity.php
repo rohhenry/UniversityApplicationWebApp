577a7fd90204e236460a15f54f25bd7f75b0b714
g9i2b <henryroh2000@gmail.com>
2020-06-14 15:10:03 -0700

    apply to university fixed

--- src/applyToUniversity.php
+++ src/applyToUniversity.php
@@ -6,15 +6,14 @@ require_once "config.php";
 
 $username = $_SESSION["username"];
 //$join = "SELECT university.name FROM university WHERE univeristy.name NOT IN (SELECT university_name FROM application, student WHERE application.student_id = student.id AND student.login_username = '$username')";
-$stmt = $mysqli->prepare("SELECT university.name FROM university WHERE university.name NOT IN (SELECT university_name FROM application, student WHERE application.student_id = student.id AND student.login_username = ?)");
+$stmt = $mysqli->prepare("SELECT Faculty.university_name, Faculty.name FROM Faculty WHERE (Faculty.university_name, Faculty.name) NOT IN (SELECT Application.university_name, Application.faculty_name FROM application, student WHERE application.student_id = student.id AND student.login_username=?)");
 $stmt->bind_param("s", $username);
 $stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($university_name);
+$stmt->bind_result($university_name, $faculty_name);
 //$result = $mysqli->query($join)->fetch_assoc();
 //echo($result[name]);
 while($stmt->fetch()){
-    echo "<b>". $university_name. "</b>";
+    echo "<b> $university_name, $faculty_name</b><br>";
 }
 
 
